4a43553abadd256193a354f863ce1a19eceb0436
john <jrbonnke@gmail.com>
2025-04-24 13:21:53 +0530

    private subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,3 +34,36 @@ resource "aws_subnet" "public_subnet_2" {
     Name = "public-subnet-1b"
   }
 }
+
+resource "aws_subnet" "app_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/28"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "app-subnet-1a"
+  }
+}
+resource "aws_subnet" "app_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/28"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "app-subnet-1b"
+  }
+}
+resource "aws_subnet" "db_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/28"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "db-subnet-1a"
+  }
+}
+resource "aws_subnet" "db_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.5.0/28"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "db-subnet-1b"
+  }
+}
