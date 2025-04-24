4a34913300ac9620bf5d8533d150840730f3b42d
john <jrbonnke@gmail.com>
2025-04-24 18:40:20 +0530

    changes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-tstate-new"
+    bucket         = "terraform-tstate"
     key            = "terraform/terraform.tstate"
     region         = "us-east-1"               
     # dynamodb_table = "terraform-locks"           
