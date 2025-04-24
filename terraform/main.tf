9c4ac7001ea212d498227b2b4c7a66390bc36c3e
john <jrbonnke@gmail.com>
2025-04-24 13:19:03 +0530

    created public subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,22 @@ resource "aws_internet_gateway" "igw" {
     Name = "three-tier-igw"
   }
 }
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/28"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-1a"
+  }
+}
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/28"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-1b"
+  }
+}
