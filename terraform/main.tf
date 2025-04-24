67113be84179d1e6a3d2e962d78d5bfc52f3293d
john <jrbonnke@gmail.com>
2025-04-24 13:07:29 +0530

    vpc changes

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+     Name = "three-tier-vpc"
+  }
+   
+  }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "three-tier-igw"
+  }
+}
