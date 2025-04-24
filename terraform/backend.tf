b55fea9827a8e3464832cc73c4c699b64f89cd41
john <jrbonnke@gmail.com>
2025-04-24 18:26:05 +0530

    backend file changes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-tstate"
-    key            = "/opt/actions-runner/_work/github-action/github-action/terraform"
+    key            = "opt/actions-runner/_work/github-action/github-action/terraform"
     region         = "us-east-1"               
     # dynamodb_table = "terraform-locks"           
     encrypt        = true
